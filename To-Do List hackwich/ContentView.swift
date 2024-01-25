00e17266450ecb8b1bc18fcc9400081c0a291e2e
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-01-25 09:33:51 -0600

    Initial Commit

---
+++ To-Do List hackwich/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  To-Do List hackwich
+//
+//  Created by Phillip Mantatsky on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To-Do List hackwich/To_Do_List_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_hackwichApp.swift
+//  To-Do List hackwich
+//
+//  Created by Phillip Mantatsky on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
